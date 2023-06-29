c3fe0f3ab527309cd2986f7227f1f90d2b1671c6
Oklard <54605502+Oklard@users.noreply.github.com>
2023-06-29 09:08:53 -0300

    Update formatCurrency.ts

--- src/ultilities/formatCurrency.ts
+++ src/ultilities/formatCurrency.ts
@@ -6,3 +6,15 @@ const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
 export function formatCurrency(number: number) {
   return CURRENCY_FORMATTER.format(number);
 }
+//Caso queira colocar um tamanho maximo de display
+// const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+//   currency: 'BRL',
+//   style: 'decimal',
+// })
+
+// export function formatCurrency(number: number): string {
+//   return number > 100000
+//     ? CURRENCY_FORMATTER.format(number).substring(0, 5) + '...'
+//     : CURRENCY_FORMATTER.format(number)
+// }
+
